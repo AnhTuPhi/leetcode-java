9e5b8e81501baa102390c09660ec27795cfec0f4
Phi Anh Tu <tu.phianh@ivnd.com.vn>
2025-01-09 09:42:50 +0700

    update p04

--- src/main/java/vn/com/leetcode/problems/p0001_p0100/s0004/Problem4.java
+++ src/main/java/vn/com/leetcode/problems/p0001_p0100/s0004/Problem4.java
@@ -82,28 +82,27 @@ public class Problem4 {
 
     public static double secondFindMedianSortedArrays(int[] nums1, int[] nums2) {
         double[] merged = new double[nums1.length + nums2.length];
-        int median = 1;
         int i = 0;
         int j = 0;
         int k = 0;
         while (i < nums1.length && j < nums2.length) {
             if (nums1[i] <= nums2[j]) {
-                merged[k] = (double) nums1[i];
+                merged[k] = nums1[i];
                 i++;
 
             } else {
-                merged[k] = (double) nums2[j];
+                merged[k] = nums2[j];
                 j++;
             }
             k++;
         }
         while (i < nums1.length) {
-            merged[k] = (double) nums1[i];
+            merged[k] = nums1[i];
             i++;
             k++;
         }
         while (j < nums2.length) {
-            merged[k] = (double) nums2[j];
+            merged[k] = nums2[j];
             j++;
             k++;
         }
