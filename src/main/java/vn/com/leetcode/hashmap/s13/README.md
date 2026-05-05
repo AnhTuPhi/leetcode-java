07a3a910820fbdbb77aec585aa88d2b939ea24a9
Phi Anh Tú <pat@MacBook-Pro-cua-Phi.local>
2026-05-06 00:10:37 +0700

    clean s13

--- src/main/java/vn/com/leetcode/hashmap/s13/README.md
+++ src/main/java/vn/com/leetcode/hashmap/s13/README.md
@@ -53,3 +53,68 @@ Explanation: M = 1000, CM = 900, XC = 90, IV = 4.
 - `1 <= s.length <= 15`
 - `s` contains only the characters `('I', 'V', 'X', 'L', 'C', 'D', 'M')`
 - It is **guaranteed** that `s` is a valid roman numeral in the range `[1, 3999]`
+
+## Cách giải
+Có thể áp dụng hash map hoặc switch case để đưa về dạng key value để mapping giá trị.
+Từ input là một string để ý là nếu phần tử sau có giá trị thấp hơn thì giá trị sẽ bị trừ và ngược lại.
+
+Ứng dụng hash map
+=> space mất công lưu thêm hash map
+```java
+
+        Map<Character, Integer> mapper = new HashMap<>();
+        mapper.put('I', 1);
+        mapper.put('V', 5);
+        mapper.put('X', 10);
+        mapper.put('L', 50);
+        mapper.put('C', 100);
+        mapper.put('D', 500);
+        mapper.put('M', 1000);
+
+        int result = 0;
+        for (int i=0; i< in.length() - 1; i++) {
+            System.out.println(i);
+            if (mapper.get(in.charAt(i)) < mapper.get(in.charAt(i + 1))) {
+                result = result - mapper.get(in.charAt(i));
+            } else {
+                result = result + mapper.get(in.charAt(i));
+            }
+        }
+        System.out.println(result + mapper.get(in.charAt(in.length() - 1)));
+```
+
+Thay vì tạo hash map, có thể dùng luôn switch case đỡ tốn bộ nhớ lưu thêm
+```java
+class Solution {
+        public int romanToInt(String s) {
+            int total = 0;
+            int prevValue = 0;
+
+            for (int i = s.length() - 1; i >= 0; i--) {
+                int currentValue = getValue(s.charAt(i));
+
+                if (currentValue < prevValue) {
+                    total -= currentValue;
+                } else {
+                    total += currentValue;
+                }
+                prevValue = currentValue;
+            }
+
+            return total;
+        }
+
+        private int getValue(char c) {
+            switch (c) {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                default: return 0;
+            }
+        }
+    }
+```
--- src/main/java/vn/com/leetcode/hashmap/s13/s13_roman_integer.java
+++ src/main/java/vn/com/leetcode/hashmap/s13/s13_roman_integer.java
@@ -8,13 +8,35 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-record Input(String in) {}
+record Input(String in) {
+}
 
 public class s13_roman_integer extends Solution<Input, Integer> {
+
+    /**
+     * Sử dụng hash map
+     * @param input
+     * @return
+     */
     @Override
     public Integer solve(Input input) {
         String in = input.in();
 
+        Map<Character, Integer> mapper = init();
+
+        int result = 0;
+        for (int i = 0; i < in.length() - 1; i++) {
+            if (mapper.get(in.charAt(i)) < mapper.get(in.charAt(i + 1))) {
+                result -= mapper.get(in.charAt(i));
+            } else {
+                result += mapper.get(in.charAt(i));
+            }
+        }
+
+        return result + mapper.get(in.charAt(in.length() - 1));
+    }
+
+    private Map<Character, Integer> init() {
         Map<Character, Integer> mapper = new HashMap<>();
         mapper.put('I', 1);
         mapper.put('V', 5);
@@ -23,17 +45,7 @@ public class s13_roman_integer extends Solution<Input, Integer> {
         mapper.put('C', 100);
         mapper.put('D', 500);
         mapper.put('M', 1000);
-
-        int result = 0;
-        for (int i=0; i< in.length() - 1; i++) {
-            if (mapper.get(in.charAt(i)) < mapper.get(in.charAt(i + 1))) {
-                result = result - mapper.get(in.charAt(i));
-            } else {
-                result = result + mapper.get(in.charAt(i));
-            }
-        }
-
-        return result;
+        return mapper;
     }
 
     @Override
@@ -46,59 +58,45 @@ public class s13_roman_integer extends Solution<Input, Integer> {
     }
 
     public static void main(String[] args) {
-        String in = "III";
-        Map<Character, Integer> mapper = new HashMap<>();
-        mapper.put('I', 1);
-        mapper.put('V', 5);
-        mapper.put('X', 10);
-        mapper.put('L', 50);
-        mapper.put('C', 100);
-        mapper.put('D', 500);
-        mapper.put('M', 1000);
+        var sol = new s13_roman_integer();
+        sol.run();
+    }
 
-        int result = 0;
-        for (int i=0; i< in.length() - 1; i++) {
-            System.out.println(i);
-            if (mapper.get(in.charAt(i)) < mapper.get(in.charAt(i + 1))) {
-                result = result - mapper.get(in.charAt(i));
+    /**
+     * Sử dụng switch casse thay cho hash map
+     * @param s
+     * @return
+     */
+    public int romanToInt(String s) {
+        int total = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = getValue(s.charAt(i));
+
+            if (currentValue < prevValue) {
+                total -= currentValue;
             } else {
-                result = result + mapper.get(in.charAt(i));
+                total += currentValue;
             }
+            prevValue = currentValue;
         }
-        System.out.println(result + mapper.get(in.charAt(in.length() - 1)));
+
+        return total;
     }
 
-    class Solution {
-        public int romanToInt(String s) {
-            int total = 0;
-            int prevValue = 0;
-
-            for (int i = s.length() - 1; i >= 0; i--) {
-                int currentValue = getValue(s.charAt(i));
-
-                if (currentValue < prevValue) {
-                    total -= currentValue;
-                } else {
-                    total += currentValue;
-                }
-                prevValue = currentValue;
-            }
-
-            return total;
-        }
-
-        private int getValue(char c) {
-            switch (c) {
-                case 'I': return 1;
-                case 'V': return 5;
-                case 'X': return 10;
-                case 'L': return 50;
-                case 'C': return 100;
-                case 'D': return 500;
-                case 'M': return 1000;
-                default: return 0;
-            }
-        }
+    private int getValue(char c) {
+        return switch (c) {
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X' -> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
+            default -> 0;
+        };
     }
+
 
 }
