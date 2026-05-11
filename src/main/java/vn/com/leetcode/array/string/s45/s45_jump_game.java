a328f5bc1ddb190e650408f1788700ad6d5df0ce
Phi Anh Tú <pat@MacBook-Pro-cua-Phi.local>
2026-05-11 22:50:38 +0700

    solve s45

--- src/main/java/vn/com/leetcode/array/string/s45/s45_jump_game.java
+++ src/main/java/vn/com/leetcode/array/string/s45/s45_jump_game.java
@@ -5,7 +5,8 @@ import vn.com.leetcode.TestCase;
 
 import java.util.List;
 
-record Input(int[] nums) {}
+record Input(int[] nums) {
+}
 
 public class s45_jump_game extends Solution<Input, Integer> {
 
@@ -13,14 +14,31 @@ public class s45_jump_game extends Solution<Input, Integer> {
     public Integer solve(Input input) {
         int[] nums = input.nums();
 
-        return 0;
+        int jumps = 0;
+        int current = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+
+            if (i == current) {
+                jumps++;
+                current = farthest;
+            }
+
+            if (current == nums.length - 1) {
+                break;
+            }
+        }
+
+        return jumps;
     }
 
     @Override
     public List<TestCase<Input, Integer>> testCases() {
         return List.of(
-                TestCase.of(new Input(new int[]{2,3,1,1,4}), 2),
-                TestCase.of(new Input(new int[]{2,3,0,1,4}), 2)
+                TestCase.of(new Input(new int[]{2, 3, 1, 1, 4}), 2),
+                TestCase.of(new Input(new int[]{2, 3, 0, 1, 4}), 2)
         );
     }
 
